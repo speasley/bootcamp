06285125dd1c844fc58b23ae181dda3ff75e10b6
mo khan <mo@mokhan.ca>
2013-10-10 20:31:54 -0600

    improve fibonacci iterator spec.

--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -1,5 +1,11 @@
 require "spec_helper"
 
+def fibonacci(n)
+end
+
+def fibonacci_enumerator
+end
+
 describe "fibonacci" do
   it "should return the first n numbers in fibonacci" do
     f = ->(x){ x < 2 ? x : f.call(x-1) + f.call(x-2) }
@@ -8,8 +14,12 @@ describe "fibonacci" do
     end
   end
 
-  xit "can enumerate forever" do
-    fibonacci_enumerator.take_while { |n| n < 1000 }
+  it "can enumerate forever" do
+    results = fibonacci_enumerator.take(18)
+    f = ->(x){ x < 2 ? x : f.call(x-1) + f.call(x-2) }
+    18.times do |n|
+      results.should include(f.call(n))
+    end
   end
 end
 
