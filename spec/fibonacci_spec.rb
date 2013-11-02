a93dc9d8f4bf21bcbeebb0fd72a37a34f653f852
mo khan <mo@mokhan.ca>
2013-11-01 20:23:18 -0600

    solve fibonacci.

--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -1,9 +1,20 @@
 require "spec_helper"
 
 def fibonacci(n)
+  n < 2 ? n : fibonacci( n - 1) + fibonacci(n - 2)
 end
 
 def fibonacci_enumerator
+  Enumerator.new do |yielder|
+    x, y = 0, 1
+    yielder.yield x
+    yielder.yield y
+    loop do
+      result = x + y
+      yielder.yield result
+      x, y = y, result
+    end
+  end
 end
 
 describe "fibonacci" do
