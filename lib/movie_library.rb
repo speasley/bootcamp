fae19ed7c9e9d88ac477859f1a2aad6a84c2e646
Stephen Peasley <s@stephenpeasley.com>
2013-11-14 19:39:37 -0700

    Sorts by studio AND year published without blowing up

--- lib/movie_library.rb
+++ lib/movie_library.rb
@@ -66,6 +66,26 @@ class MovieLibrary
   def sort_movies_by_title_ascending()
     @movies.sort {|a,b| a.title <=> b.title}
   end
+  
+  def sort_movies_by_descending_release_date()
+    @movies.sort {|a,b| b.year_published <=> a.year_published}
+  end
+  
+  def sort_movies_by_ascending_release_date()
+    @movies.sort {|a,b| a.year_published <=> b.year_published}
+  end
+  
+  def sort_movies_by_preferred_studios_and_release_date_ascending()
+    preferred_order = [Studio::Pixar, Studio::Disney, Studio::CastleRock, Studio::MiramaxFilms, Studio::RegencyEnterprises]
+    sorted = @movies.sort do |movie1,movie2|  
+      new_order = preferred_order.find_index(movie1.studio) <=> preferred_order.find_index(movie2.studio)
+      if(new_order==0)
+        movie1.year_published <=> movie2.year_published
+      else
+        new_order
+      end
+    end
+  end
 
   def each
     # @movies.count.times do |n|
